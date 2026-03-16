7af03e2a337b244c4cf4c362318205ec893645a6
andre <aperazanascimento@danielcastelao.org>
2026-03-16 14:40:58 +0100

    commit 6

--- nave.py
+++ nave.py
@@ -1,18 +1,18 @@
 class Nave:
-    def __init__(self, nombre, tipo, tamano):
+    def __init__(self, nombre, tipo,vida):
         self.nombre = nombre
         self.tipo = tipo
-        self.vida = tamano
+        self.vida = vida
         self.hundido = False
         self.TOCADO = 1
         self.HUNDIDO = 2
 
-
     def recibir_disparo(self):
         self.vida -= 1
-
         if self.vida <= 0:
             self.hundido = True
-            return self.hundido
+            print(f"{self.nombre} hundido")
+            return self.HUNDIDO
         else:
+            print(f"{self.nombre} tocado. Vida restante: {self.vida}")
             return self.TOCADO
