bff037c05b5632e7227d6019c34ed46df7fe1da1
andre <aperazanascimento@danielcastelao.org>
2026-03-23 14:33:46 +0100

    corregido

--- casilla.py
+++ casilla.py
@@ -1,2 +1,32 @@
-class casilla:
-    def __init__(self):
+from nave import Nave
+
+class Casilla:
+    def __init__(self, nave):
+        self.nave = nave
+        self.disparada = False
+
+    def recibir_disparo(self):
+        if self.disparada == True:
+            if self.nave != 'agua':
+                print(f'Casilla ya disparada con {self.nave.nombre},de tipo {self.nave.tipo},con {self.nave.vida} vidas ')
+                self.disparada = True
+
+            else:
+                print( f'Casilla ya disparada es de agua')
+                self.disparada = True
+
+
+        if self.nave == 'agua':
+           self.disparada = True
+           return 0
+
+        if self.disparada == False:
+            resultado = self.nave.recibir_disparo()
+            self.disparada = True
+            return resultado
+
+
+
+
+
+
--- juego.py
+++ juego.py
@@ -1,12 +1,14 @@
 from tablero import Tablero
+
 class Juego:
     def __init__(self):
         self.tablero = Tablero()
+        self.lanzar_ataque(1, 0)
+        self.lanzar_ataque(1, 0)
         self.lanzar_ataque(1, 1)
         self.lanzar_ataque(1, 2)
-        self.lanzar_ataque(1, 3)
+        self.lanzar_ataque(1, 1)
         self.lanzar_ataque(1, 4)
-        self.lanzar_ataque(1, 5)
 
 
     def mostrar_resultado(self, resultado):
@@ -19,7 +21,10 @@ class Juego:
 
     def lanzar_ataque(self, x, y):
         print(f"Ataque a {x},{y}")
-        resultado = self.tablero.comprobar_impacto(x, y)
+        casilla = self.tablero.casillero[x][y]
+
+        resultado = casilla.recibir_disparo()
+
         self.mostrar_resultado(resultado)
 
 if __name__ == "__main__":
